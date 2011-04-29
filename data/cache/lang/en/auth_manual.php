99bed937d9edc4eb1378ba4c76194fc553285966
sean <sean@sean-1982>
2011-04-29 13:07:55 -0700

    wiped cache, needed to commit pull again

--- data/cache/lang/en/auth_manual.php
+++
@@ -1,4 +0,0 @@
-<?php $this->cache['en']['auth_manual'] = array (
-  'auth_manualdescription' => 'This method removes any way for users to create their own accounts.  All accounts must be manually created by the admin user.',
-  'pluginname' => 'Manual accounts',
-);
